cbe47be0468d492ea4b6236f43920110119bf863
sylhare <sylhare@outlook.com>
2021-02-26 09:15:55 -0500

    Update examples

--- src/album_aggregate_advanced.sql
+++ src/album_aggregate_advanced.sql
@@ -26,3 +26,13 @@ SELECT a.title AS Album, COUNT(t.track_number) as Tracks
   HAVING Tracks >= 10
   ORDER BY Tracks DESC, Album
 ;
+
+-- Defined functions
+
+SELECT SEC_TO_TIME(320);
+SELECT TIME_TO_SEC('5:20');
+SELECT title, duration, SEC_TO_TIME(duration) FROM track;
+
+SELECT a.title, SUM(duration), SUM_SEC_TO_TIME(t.duration) FROM track AS t
+  JOIN album AS a ON t.album_id = a.id
+  GROUP BY a.id
--- src/world_string.sql
+++ src/world_string.sql
@@ -23,3 +23,23 @@ SELECT UPPER(Name) FROM City ORDER BY Name;
 SELECT LOWER(Name) FROM City ORDER BY Name;
 
 
+-- Simple subselect
+
+CREATE TABLE t ( a TEXT, b TEXT );
+INSERT INTO t VALUES ( 'NY0123', 'US4567' );
+INSERT INTO t VALUES ( 'AZ9437', 'GB1234' );
+INSERT INTO t VALUES ( 'CA1279', 'FR5678' );
+SELECT * FROM t;
+
+SELECT SUBSTR(a, 1, 2) AS State, SUBSTR(a, 3) AS SCode, 
+  SUBSTR(b, 1, 2) AS Country, SUBSTR(b, 3) AS CCode FROM t;
+
+SELECT co.Name, ss.CCode FROM (
+    SELECT SUBSTR(a, 1, 2) AS State, SUBSTR(a, 3) AS SCode,
+      SUBSTR(b, 1, 2) AS Country, SUBSTR(b, 3) AS CCode FROM t
+  ) AS ss
+  JOIN Country AS co
+    ON co.Code2 = ss.Country
+;
+
+DROP TABLE t;
